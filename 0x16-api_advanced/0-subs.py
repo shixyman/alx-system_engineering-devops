86bbdba981109e2e5779d929cbcb1917f7c8bc18
Y'Usri R'Chid <shixymanflowx@gmail.com>
2024-02-13 15:49:59 +0100

    testing

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,14 +5,21 @@ Contains the number_of_subscribers function
 import requests
 
 def number_of_subscribers(subreddit):
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "My Reddit API Client"}
+    # Set a custom User-Agent to avoid Too Many Requests error
+    headers = {'User-Agent': 'My Reddit API Client'}
 
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    # Make the API request to retrieve subreddit information
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    response = requests.get(url, headers=headers)
 
+    # Check if the request was successful
     if response.status_code == 200:
-        data = response.json()
-        subscribers = data["data"]["subscribers"]
-        return subscribers
+        try:
+            data = response.json()
+            return data['data']['subscribers']
+        except KeyError:
+            # Invalid subreddit
+            return 0
     else:
+        # Invalid subreddit or other error
         return 0
