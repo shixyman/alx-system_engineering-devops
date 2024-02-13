04aa20bf05a88ea76f00bd16db1441686583fc9a
Y'Usri R'Chid <shixymanflowx@gmail.com>
2024-02-13 18:20:23 +0100

    modify

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,30 +1,27 @@
 #!/usr/bin/python3
 """Module for task 2"""
 
+import requests
 
-def recurse(subreddit, hot_list=[], count=0, after=None):
-    """Queries the Reddit API and returns all hot posts
-    of the subreddit"""
-    import requests
-
-    sub_info = requests.get(
-        "https://www.reddit.com/r/{}/hot.json".format(subreddit),
-        params={"count": count, "after": after},
-        headers={"User-Agent": "My-User-Agent"},
-        allow_redirects=False,
-    )
-    if sub_info.status_code >= 400:
+def recurse(subreddit, hot_list=[]):
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    headers = {"User-Agent": "My Reddit API Client"}  # Set a custom User-Agent
+    
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    
+    if response.status_code == 200:
+        data = response.json()
+        children = data["data"]["children"]
+        
+        for child in children:
+            title = child["data"]["title"]
+            hot_list.append(title)
+        
+        after = data["data"]["after"]
+        
+        if after:
+            return recurse(subreddit, hot_list=hot_list)
+        else:
+            return hot_list
+    else:
         return None
-
-    hot_l = hot_list + [
-        child.get("data").get("title")
-        for child in sub_info.json().get("data").get("children")
-    ]
-
-    info = sub_info.json()
-    if not info.get("data").get("after"):
-        return hot_l
-
-    return recurse(
-        subreddit, hot_l, info.get("data").get("count"), info.get("data").get("after")
-    )
