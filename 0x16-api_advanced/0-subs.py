9a1b790e6f7020ad41038a9c4e352f1d7783c4b9
Y'Usri R'Chid <shixymanflowx@gmail.com>
2024-02-14 03:18:45 +0100

    modify

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,19 +8,15 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """ Returns subscriber count of subreddit or 0 """
+    """Returns subscriber count of subreddit or 0"""
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'user-agent': 'my-app/0.0.1'}
+    headers = {"User-Agent": "my-app/0.0.1"}
 
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
         response.raise_for_status()
         data = response.json()
-
-        if 'subscribers' in data.get('data', {}):
-            return data['data']['subscribers']
-
+        subscribers = data["data"].get("subscribers")
+        return subscribers if subscribers is not None else 0
     except (requests.RequestException, ValueError):
-        pass
-
-    return 0
+        return 0
